591f0de18416c0cda8227b8d66b6b9728c76881f
Ticha <tichagodwill1@gmail.com>
2023-02-12 23:39:14 +0300

    adding footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
+import Footer from "./components/Layout/Footer";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -20,6 +21,7 @@ function App() {
       <main>
         <Meals />
       </main>
+      <Footer />
     </CartProvider>
   );
 }
---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { FaFacebook, FaInstagram, FaWhatsapp, FaYoutube } from "react-icons/fa";
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={classes.footer}>
+      <main className={classes.footer__content}>
+        <section className={classes.footer__section}>
+          <h2>Reach Us</h2>
+          <section className={classes["icons-section"]}>
+            <a href="/" className={classes.facebook}>
+              <FaFacebook className={classes.icon} />
+            </a>
+            <a href="/" className={classes.youtube}>
+              <FaYoutube className={classes.icon} />
+            </a>
+            <a href="/" className={classes.instagram}>
+              <FaInstagram className={classes.icon} />
+            </a>
+            <a href="/" className={classes.whatsapp}>
+              <FaWhatsapp className={classes.icon} />
+            </a>
+          </section>
+        </section>
+        <section className={classes.footer__section}>
+          <h2>Events</h2>
+          <ul className={classes.list__items}>
+            <a href="/">
+              <li className={classes.listed__item}>Team</li>
+            </a>
+            <a href="/">
+              <li className={classes.listed__item}>Branches</li>
+            </a>
+            <a href="/">
+              <li className={classes.listed__item}>Healthy Food</li>
+            </a>
+            <a href="/">
+              <li className={classes.listed__item}>Careers</li>
+            </a>
+          </ul>
+        </section>
+        <section className={classes.footer__section}>
+          <section className={classes.newsletter}>
+            <form>
+              <strong>
+                <p>Signup for our newsletter</p>
+              </strong>
+              <input type="email" label="Email address" />
+              <button className={classes.btn}>
+                <strong>Subscribe</strong>
+              </button>
+            </form>
+          </section>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Footer;
